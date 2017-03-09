87090009bc1274f1281120507a45d3e497c2fe08
andrew0golus <andrew.golus@yale.edu>
2017-03-09 00:02:14 -0500

    Delete plot1.R

--- plot1.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(lubridate)
-setwd("C:/Users/ag827/Desktop/R")
-path <- "./Exploratory Data Analysis/Course Project 1/household_power_consumption.txt"
-epc <- read.table(path, header = TRUE, sep = ";")
-epc_2days <- filter(epc, paste(epc$Date) %in% c("2/2/2007", "1/2/2007"))
-x <- as.numeric(paste(epc_2days$Global_active_power))
-hist(x, col = "red", breaks = 15, main = "Global Active Power", xlab = "Global Active Power (killowats)")
-dev.copy(png, file = "plot1.png")
